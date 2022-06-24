78c5b40fe8a512ca6bec213dd26aa3f554bf18cb
angle-zhang <angelazhang1220@gmail.com>
2022-06-24 16:47:08 -0700

    added outlier extraction and list of studies

--- data-raw/Check_outliers.R
+++ data-raw/Check_outliers.R
@@ -8,8 +8,26 @@ rm(list = ls())
 # Packages
 library(tidyverse)
 
-# Load data 
-afcd <- load(file='data/afcd.rda') 
+outdir <- "data-raw/processed"
+datadir <- "data"
 
-# for each nutrient give a boxplot of values, see if any specific studies or species keep showing up as outliers
-# note studies and species that show up as outliers in this document
+# Load data 
+load(file.path(datadir, "afcd.rda"))
+afcd_ref <- readRDS(file.path(outdir, "AFCD_reference_key.Rds"))
+
+afcd_edible = afcd %>%
+  mutate(edible_value = value * edible_prop)
+
+# get relevant outliers in groups of nutrients and units
+# group by nutrient units as well since some nutrients could have different units
+# ideally, they would all be uniform, but we have to convert them still 
+nutrient_outliers =  afcd_edible %>%
+  group_by(nutrient, nutrient_units) %>%
+  rstatix::identify_outliers(edible_value) %>%
+  select(study_id, nutrient, value, sciname, genus, family, order, class, phylum, kingdom, common_name) 
+
+# add to this list of outlierss
+study_outliers = nutrient_outliers %>%
+  count(study_id) %>%
+  merge(afcd_ref, by="study_id") %>%
+  arrange(desc(n))
